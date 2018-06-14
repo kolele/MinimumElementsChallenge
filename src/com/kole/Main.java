cbde95330d521ab56eb390924fc963fe03118ca3
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-14 18:05:52 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -1,8 +1,32 @@
 package com.kole;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(findMin(readIntegers(3)));
     }
+
+    public static int[] readIntegers(int counter){
+        int[] tempArray = new int[counter];
+        for (int i=0;i<tempArray.length;i++){
+            System.out.println("Enter an integer to populate array: ");
+            tempArray[i]= scanner.nextInt();
+        }return tempArray;
+
+    }
+
+    public static int findMin(int[] array){
+        int minValue = Integer.MAX_VALUE;
+        for (int i=0;i<array.length;i++){
+            if (array[i]<minValue){
+                minValue=array[i];
+            }
+        }return minValue;
+    }
+
+
 }
